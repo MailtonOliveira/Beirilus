bb70711fcd37257f39a3b4dfa69b23a0da336099
Mailton-Oliveira <mailton.s.oliveira@gmail.com>
2022-12-15 14:23:56 -0300

    REFACTOR: corrigindo semantica schema.prima e adicionando senha criptorgrafada em usercontroller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,28 +1,31 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
+
 
 const prisma = new PrismaClient();
 
 class userController {
-
   async listUsers(req: Request, res: Response, next: NextFunction) {
     try {
-        const listingUsers = await prisma.user.findMany();
-        res.json({ listingUsers });
+      const listingUsers = await prisma.user.findMany();
+      res.json({ listingUsers });
     } catch (error) {
-        next(error);
+      next(error);
     }
-  };
+  }
 
   async createUser(req: Request, res: Response, next: NextFunction) {
     try {
-      const { email, name, phone, birth } = req.body;
+      const { email, name, phone, birth, passwd } = req.body;
+      const newPass = bcrypt.hashSync(passwd, 10);
       const userCreate = await prisma.user.create({
         data: {
           email,
           name,
           phone,
           birth,
+          passwd: newPass
         },
       });
       res.status(201).json(userCreate);
