21d1bb39f66587dd4be60d614a9066505fde83b4
Mailton-Oliveira <mailton.s.oliveira@gmail.com>
2022-12-21 23:21:48 -0300

    FEAT: adicionando typeUser REFACTOR: atalizando employee

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,55 +1,59 @@
-
 import { Request, Response, NextFunction } from "express";
-import prisma from "../database/prismaClient"
-
+import bcrypt from "bcrypt";
+import prisma from "../database/prismaClient";
 
 class employeeController {
-
   async listEmployees(req: Request, res: Response, next: NextFunction) {
     try {
-        const listingEmployees = await prisma.employee.findMany();
-        res.json({ listingEmployees });
+      const listingEmployees = await prisma.employee.findMany();
+      res.json({ listingEmployees });
     } catch (error) {
-        next(error);
+      console.log(error);
     }
-  };
+  }
 
   async oneEmployee(req: Request, res: Response, next: NextFunction) {
     try {
+      const { id } = req.params;
 
-        const { id } = req.params;
-
-        const employee = await prisma.employee.findUnique({
-            where: {
-                id,
-            }
-        });
-
-        if (!employee) {
-            res.status(404).json("id não encontrado")
-        };
-
-        res.status(200).json(employee)
-
-    } catch (error) {
-        next(error)
-    }
-
-  };
-
-  async createEmployee(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { userId } = req.body;
-      const employeeCreate = await prisma.employee.create({
-        data: {
-          userId,
+      const employee = await prisma.employee.findUnique({
+        where: {
+          id,
         },
       });
-      res.status(201).json(employeeCreate);
+
+      if (!employee) {
+        res.status(404).json("id não encontrado");
+      }
+
+      res.status(200).json(employee);
     } catch (error) {
       next(error);
     }
-  };
-}
+  }
 
+  async createEmployee(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, name, phone, birth, passwd, employee, typeUser } = req.body;
+      const newPass = bcrypt.hashSync(passwd, 10);
+      const employeeCreate = await prisma.user.create({
+        data: {
+          email,
+          name,
+          phone,
+          birth,
+          passwd: newPass,
+          employee,
+          typeUser
+        }
+      });
+               
+        
+      
+      res.status(201).json(employeeCreate);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 export default employeeController;
--- src/validations/employees/create.ts
+++ src/validations/employees/create.ts
@@ -2,6 +2,14 @@ import { validate, Joi } from "express-validation";
 
 export default validate({
     body: Joi.object({
-        userId: Joi.string().required(),
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        phone: Joi.number().required(),
+        birth: Joi.date().required(),
+        passwd: Joi.string().min(8).required(),
+        // typeUserId: Joi.string().hex().required(),
+        employee: Joi.object({
+            
+        })
     }),
 });
