709a608a34fbf5a3b5eb00cc216a74a578f367a5
Erikwpp <erik_wpp@hotmail.com>
2023-01-04 18:30:51 -0300

    FEAT: adicionando endpoints Booking, Controllers, validations e routers.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import typeController from './../controllers/typeUserController';
 import userController from './../controllers/usersController';
 import employeeController from '../controllers/employeeController';
+import bookingController from '../controllers/bookingController';
 
 
 import express from "express";
@@ -12,6 +13,10 @@ import userUpdateValidation from '../validations/users/update';
 import employeeCreateValidation from '../validations/employees/create';
 import employeeOneValidation from '../validations/employees/getOne';
 
+import bookingCreateValidation from '../validations/booking/create';
+import bookingOneValidation from '../validations/booking/getOne';
+import bookingUpdateValidation from '../validations/booking/update';
+
 
 const routes = express.Router();
 
@@ -19,9 +24,10 @@ const user = new userController;
 
 const employee = new employeeController;
 const typeUser = new typeController;
+const booking = new bookingController;
 
 routes.get("/clients", user.listUsers);
-routes.post("/clients", user.createUser);
+routes.post("/clients", userCreateValidation, user.createUser);
 routes.get("/clients/:id", userOneValidation, user.oneUser);
 routes.put("/clients/:id", userUpdateValidation, user.updateUser);
 routes.delete("/clients/:id", userOneValidation, user.deleteUser);
@@ -33,5 +39,10 @@ routes.get("/employees/:id", employeeOneValidation, employee.oneEmployee );
 routes.post("/typeUser", typeUser.createType);
 routes.get("/typeUser", typeUser.listTypes);
 
+routes.get("/booking", booking.listBooking);
+routes.post("/booking", bookingCreateValidation, booking.createBooking);
+routes.get("/booking/:id", bookingOneValidation, booking.findByIdBooking);
+routes.put("/booking/:id", bookingUpdateValidation, booking.updateBooking);
+routes.delete("/booking/:id", booking.deleteBooking);
 
 export default routes;
