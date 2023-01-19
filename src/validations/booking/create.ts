876aae2ed063ab1234cfc78f1540ff20bb51ef71
Nayara Duarte de Sousa <nayadsousa@gmail.com>
2023-01-19 20:10:27 -0300

    FEAT: corrigindo a semântica do validation bookin

--- src/validations/booking/create.ts
+++ src/validations/booking/create.ts
@@ -2,8 +2,8 @@ import { validate, Joi } from "express-validation";
 
 export default validate({
     body: Joi.object({
-        start: Joi.string().required(),
-        end: Joi.string().required(),
+        startDate: Joi.string().required(),
+        endDate: Joi.string().required(),
         customerId: Joi.string().hex().required(),
         servicesId: Joi.string().hex().required(),
         barberId: Joi.string().hex().required()
